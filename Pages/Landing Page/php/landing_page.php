b740bfdd3f2b927e4ae334ee68a0cecd180cf825
ashantimae16 <2023304056@pampangastateu.edu.ph>
2025-11-16 10:50:14 +0800

    Added Fade Animations

--- Pages/Landing Page/php/landing_page.php
+++ Pages/Landing Page/php/landing_page.php
@@ -223,6 +223,16 @@
     });
 });
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+});
+
+document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
+
 
 
 </script>
