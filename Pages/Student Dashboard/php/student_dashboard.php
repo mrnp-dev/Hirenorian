8cc441aa2e91d68c30388ace85f8229b42753369
ashantimae16 <2023304056@pampangastateu.edu.ph>
2025-11-20 15:02:06 +0800

    studentdashboard php file

---
+++ Pages/Student Dashboard/php/student_dashboard.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+    <aside class="sidebar">
+        <figure class="profile">
+        <img src="profile.jpg" alt="User profile photo">
+        <figcaption>Isabella Sicat</figcaption>
+        </figure>
+
+        <nav class="menu">
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Internship</a></li>
+                <li><a href="#">Help</a></li>
+            </ul>
+        </nav>
+
+        <figure class="logo">
+            <img src="logo.png" alt="School logo">
+        </figure>
+
+    </aside>
+ 
+    <main>
+        
+        <section id="dashboard-section">
+            <h1>Dashboard</h1>
+            <div id="yellow-welcome-banner">
+                <p>Hi Isabella Sicat! Wecome Back!</p>
+                <h1>Check your Job Applications</h1>
+                <div id="girl-in-toga">
+                    <img src="" alt="">
+                </div>
+
+            </div>
+        </section>
+
+        <section id="applications-section">
+            <div id="section-header">
+                <h1>Applications</h1>
+                <a href="">See All</a>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Date</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>
+                            <strong>Job Title</strong>
+                            <small>Company Name</small>
+                        </td>
+                        <td>Position</td>
+                        <td>12/24/25</td>
+                        <td>Accepted</td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <strong>Job Title</strong>
+                            <small>Company Name</small>
+                        </td>
+                        <td>Position</td>
+                        <td>12/24/25</td>
+                        <td>Accepted</td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <strong>Job Title</strong>
+                            <small>Company Name</small>
+                        </td>
+                        <td>Position</td>
+                        <td>12/24/25</td>
+                        <td>Accepted</td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            <strong>Job Title</strong>
+                            <small>Company Name</small>
+                        </td>
+                        <td>Position</td>
+                        <td>12/24/25</td>
+                        <td>Accepted</td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+        </section>
+
+        <section id="recommended-for-you">
+            <h1>Recommended For You</h1>
+            
+            <section id="top-section">
+
+                <section class="left-cards">
+                    <div class="image-section" >
+                        <img src="" alt="">
+                    </div>
+                    <section class="information-section">
+                            <section class="title-container">
+                                <h1 class="title">Architecture</h1>
+                                <h1 class="sub-title">Archi Corporation</h1>
+                             </section>
+                             <section class="button-container">
+                                <button>Internship</button>
+                             </section>                   
+                    </section>
+
+                    </section>
+                        <section class="info">
+                            <p>Super brief description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut est tincidunt, suscipit</p>
+                        </section>
+                    </section>
+                </section>
+
+                <section class="right-cards">
+                    <div class="image-section" >
+                        <img src="" alt="">
+                    </div>
+                    <section class="information-section">
+                            <section class="title-container">
+                                <h1 class="title">Architecture</h1>
+                                <h1 class="sub-title">Archi Corporation</h1>
+                             </section>
+                             <section class="button-container">
+                                <button>Internship</button>
+                             </section>                   
+                    </section>
+
+                    </section>
+                        <section class="info">
+                            <p>Super brief description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut est tincidunt, suscipit</p>
+                        </section>
+                    </section>
+                </section>
+
+  
+            </section>
+
+            <section class="bottom-section">
+
+                <section class="left-cards">
+                    <div class="image-section" >
+                        <img src="" alt="">
+                    </div>
+                    <section class="information-section">
+                            <section class="title-container">
+                                <h1 class="title">Architecture</h1>
+                                <h1 class="sub-title">Archi Corporation</h1>
+                             </section>
+                             <section class="button-container">
+                                <button>Internship</button>
+                             </section>                   
+                    </section>
+
+                    </section>
+                        <section class="info">
+                            <p>Super brief description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut est tincidunt, suscipit</p>
+                        </section>
+                    </section>
+                </section>
+
+                <section class="right-cards">
+                    <div class="image-section" >
+                        <img src="" alt="">
+                    </div>
+                    <section class="information-section">
+                            <section class="title-container">
+                                <h1 class="title">Architecture</h1>
+                                <h1 class="sub-title">Archi Corporation</h1>
+                             </section>
+                             <section class="button-container">
+                                <button>Internship</button>
+                             </section>                   
+                    </section>
+
+                    </section>
+                        <section class="info">
+                            <p>Super brief description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut est tincidunt, suscipit</p>
+                        </section>
+                    </section>
+                </section>
+
+  
+            </section>
+
+        </section>
+
+        <section>
+            
+        </section>
+
+    </main>
+
+</body>
+</html>
