4606455cb41439de18e9f5c7b548b451f5f42cd8
mrnp-dev <markreyninopascual@gmail.com>
2025-11-30 16:35:35 +0800

    Login starts session and redirects to dashboard

--- Pages/Account Registration Pages/Student Registration Page/js/student_registrationForm.js
+++ Pages/Account Registration Pages/Student Registration Page/js/student_registrationForm.js
@@ -119,6 +119,12 @@ async function check_LogIn_Fields() {
             const data = await response.json();
             if (response.ok && data.status === "success") {
                 ToastSystem.show('Login Successfully', "success");
+                setTimeout(() => {
+                    ToastSystem.show("Redirecting to Student Dashboard", "info");
+                    setTimeout(() => {
+                        window.location.href = "../../../Student%20Pages/Student%20Dashboard%20Page/php/student_dashboard.php";
+                    }, 1500);
+                }, 1500);
             } else {
                 ToastSystem.show('Login Failed', "error");
             }
